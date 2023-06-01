df785a6c492a2db9d78b955b3e62967874a1c3a4
rafacu-dev <rafaproyectosfuturos@gmail.com>
2023-06-01 02:25:36 -0400

    n

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
+import re
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, serve
+
+def staticProduction(prefix, view=serve, **kwargs):
+    if settings.DEBUG:
+        return []
+    return [
+        re_path(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), view, kwargs=kwargs),
+    ]
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
@@ -22,4 +30,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticProduction(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
